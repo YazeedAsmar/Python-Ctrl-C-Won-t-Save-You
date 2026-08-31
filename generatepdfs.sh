c20cbe1d363f897766014b718fab28fe09983c56
Yazeed Alasmar <yazeedasmar4@gmail.com>
2026-08-31 21:18:05 +0300

    Keep only notebooks

--- generatepdfs.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Script to convert jupyter notebooks to PDF files. The pdfs are saved in ./pdfs folder
-
-files=$(find . -name '*.ipynb')
-for f in $files; do
-    echo Converting: $f
-    jupyter nbconvert --log-level=0 --output-dir='./pdfs' --to pdf $f > /dev/null
-done
